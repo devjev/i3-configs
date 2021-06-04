e437a6152aa30e8d5378483f486993f59ff766a6
Jevgeni Tarasov <jevgeni@hey.com>
2021-06-04 10:50:57 +0200

    Initial commit

---
+++ put.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Get i3 Settings
+pushd i3
+rsync * ~/.config/i3
+popd
+
+# Get i3status-rs Settings
+pushd i3status-rs
+rsync * ~/.config/i3status-rust
+popd
+
+# Get rofi Settings
+pushd rofi
+rsync * ~/.config/rofi
+popd
